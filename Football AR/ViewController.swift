70d5c3e59128bce652b31560e5f1eefd400b8285
Artem Levchenko <artem.levchenko@ukma.edu.ua>
2023-06-14 23:55:04 +0300

    Initial Commit

---
+++ Football AR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football AR
+//
+//  Created by ARTEM on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
